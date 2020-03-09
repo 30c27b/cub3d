e244935a9e341efd7d5dde4629977c8eaff8dec8
Antoine Coulon <ancoulon@student.s19.be>
2020-03-09 12:42:49 +0100

    added instance initialisation method

--- sources/classes/t_inst/inst_init.c
+++ sources/classes/t_inst/inst_init.c
@@ -1,22 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   t_inst.h                                           :+:      :+:    :+:   */
+/*   inst_init.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/09 11:07:54 by ancoulon          #+#    #+#             */
-/*   Updated: 2020/03/09 12:42:24 by ancoulon         ###   ########.fr       */
+/*   Created: 2020/03/09 12:37:18 by ancoulon          #+#    #+#             */
+/*   Updated: 2020/03/09 12:42:10 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-typedef struct	s_inst
+t_inst		inst_init(t_map	map, t_win win)
 {
-	void		*mlx_ptr;
-	t_win		win;
-	t_map		map;
-}				t_inst;
+	t_inst	instance;
 
-t_inst			inst_init(t_map	map, t_win win);
+	if (!(instance.mlx_ptr = mlx_init()))
+		exit(EXIT_FAILURE);
+	instance.map = map;
+	instance.win = win;
+	return (instance);
+}
