c2519fc6e38271be9f2621f931887e6074eb5b4d
Antoine Coulon <ancoulon@student.s19.be>
2020-03-09 12:29:35 +0100

    added rbg_init proto

--- includes/classes/t_rgb.h
+++ includes/classes/t_rgb.h
@@ -6,7 +6,7 @@
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/09 11:07:51 by ancoulon          #+#    #+#             */
-/*   Updated: 2020/03/09 12:25:44 by ancoulon         ###   ########.fr       */
+/*   Updated: 2020/03/09 12:28:53 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,5 @@ typedef struct	s_rgb
 	t_uint8		b;
 }				t_rgb;
 
-
-
+t_rgb			rgb_init(t_uint8 r, t_uint8 g, t_uint8 b);
 t_int32			rgb_toint(t_rgb color);
-
