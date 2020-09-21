ba2092d398b754823bb95cee8cf4f4c741d323a7
ancoulon <ancoulon@student.s19.be>
2020-09-21 15:21:24 +0200

    update to map_init, wip

--- sources/classes/t_map/map_init.c
+++ sources/classes/t_map/map_init.c
@@ -6,7 +6,7 @@
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/09 14:09:01 by ancoulon          #+#    #+#             */
-/*   Updated: 2020/08/20 13:03:55 by ancoulon         ###   ########.fr       */
+/*   Updated: 2020/09/21 15:18:23 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,20 @@ static void	parse(t_file file, t_map *map)
 	t_list	*line;
 	t_int32	lines;
 	char	*content;
-	t_uint8 el_flag[8];
+	t_uint8 el_flag;
 
 	line = file.data;
 	lines = 0;
-	ft_bzero(el_flag, 8);
-	while (line && line->content)
-	{
-		content = (char *)line->content;
-		if (!content[0])
-		{
+	el_flag = 0b00000000;
+	// while (line && line->content)
+	// {
+	// 	content = (char *)line->content;
+	// 	if (!content[0])
+	// 	{
 
-		}
-		line = line->next;
-	}
+	// 	}
+	// 	line = line->next;
+	// }
 }
 
 t_map		map_init(t_file file)
--- sources/classes/t_map/map_init2.c
+++ sources/classes/t_map/map_init2.c
@@ -6,30 +6,39 @@
 /*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/20 11:15:41 by ancoulon          #+#    #+#             */
-/*   Updated: 2020/08/20 12:56:52 by ancoulon         ###   ########.fr       */
+/*   Updated: 2020/09/21 15:18:17 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-static void	parse_type(char *line, t_map *map, t_uint8 el_flag[8])
+static void inc_flag(t_uint8 el_flag, t_uint8 bitflag)
+{
+	if (el_flag & bitflag)
+		err_exit(ERRTYPE_BADMAP);
+	el_flag |= bitflag;
+}
+
+static void	parse_type(char *line, t_map *map, t_uint8 el_flag)
 {
 	if (!ft_strncmp(line, "R", 1))
-		el_flag[0] += 1;
+		inc_flag(el_flag, BIT_0);
 	else if (!ft_strncmp(line, "NO", 2))
-		el_flag[0] += 1;
+		inc_flag(el_flag, BIT_0);
 	else if (!ft_strncmp(line, "SO", 2))
-		el_flag[0] += 1;
+		inc_flag(el_flag, BIT_0);
 	else if (!ft_strncmp(line, "WE", 2))
-		el_flag[0] += 1;
+		inc_flag(el_flag, BIT_0);
 	else if (!ft_strncmp(line, "EA", 2))
-		el_flag[0] += 1;
+		inc_flag(el_flag, BIT_0);
 	else if (!ft_strncmp(line, "S", 1))
-		el_flag[0] += 1;
+		inc_flag(el_flag, BIT_0);
 	else if (!ft_strncmp(line, "F", 1))
-		el_flag[0] += 1;
+		inc_flag(el_flag, BIT_0);
 	else if (!ft_strncmp(line, "C", 1))
-		el_flag[0] += 1;
+		inc_flag(el_flag, BIT_0);
+	else
+		err_exit(ERRTYPE_BADMAP);
 }
 
 void 		map_init_parse_el(char *line, t_map *map, t_uint8 el_flag[8])
